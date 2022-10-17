a4cc2174802aa322cfb456d87af9cdca77a400a3
phamhuyenkr <huyenjkx@gmail.com>
2022-10-17 21:41:32 +0700

    add authentication file for login and logout Admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+//Admin -------------------------------Admin-------------------Admin---------------------
+
+//login
+Route::get('admin/login', [LoginController::class, 'getLogin'])->name('login');
+Route::post('admin/postlogin', [LoginController::class, 'postLogin'])->name('postlogin');
+
+//logout
+Route::post('admin/logout', [LogoutController::class, 'getLogout'])->name('logout');
+
+//Auth
+Route::middleware(['auth'])->group(function(){
+    Route::get('admin', [HomeController::class, 'index'])->name('home');   
+    Route::get('admin/main',[HomeController::class,'index']);   
+ });
